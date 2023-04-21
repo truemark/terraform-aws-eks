fd2f4f2232ec6f1f08b8598c4bceb55474191c6d
v47 <victor.verg@hotmail.com>
2023-04-22 00:31:41 +0200

    test

--- main.tf
+++ main.tf
@@ -206,43 +206,43 @@ resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
 }
 
 //https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases
-resource "helm_release" "aws_load_balancer_controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
 
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "image.repository"
-    value = format("602401143452.dkr.ecr.%s.amazonaws.com/amazon/aws-load-balancer-controller", data.aws_region.current.name)
-  }
+#   set {
+#     name  = "image.repository"
+#     value = format("602401143452.dkr.ecr.%s.amazonaws.com/amazon/aws-load-balancer-controller", data.aws_region.current.name)
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = true
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = true
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.aws_load_balancer_controller.arn
-    type  = "string"
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.aws_load_balancer_controller.arn
+#     type  = "string"
+#   }
 
-  set {
-    name  = "image.tag"
-    value = "v2.4.4"
-  }
-}
+#   set {
+#     name  = "image.tag"
+#     value = "v2.4.4"
+#   }
+# }
 
 module "vpc_cni_irsa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
