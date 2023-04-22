d02dd43e7f425e02f31202c6c69d2aad11bcfb70
v47 <victor.verg@hotmail.com>
2023-04-22 04:47:34 +0200

    pin down the aws lb collector chart version to 1.4.5

--- main.tf
+++ main.tf
@@ -211,6 +211,7 @@ resource "helm_release" "aws_load_balancer_controller" {
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
+  version    = "1.4.5"
 
   set {
     name  = "clusterName"
