3397570abf495097d1e0fa5242b6620b986202d1
d3vb0xx <pjain@truemark.io>
2023-04-27 18:43:17 +0530

    add addition cluster details to output

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,21 @@ output "cluster_id" {
   value       = module.eks.cluster_id
 }
 
+output "custer_name" {
+  description = "The name of the EKS cluster"
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint of the Kubernetes API server"
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value = module.eks.cluster_certificate_authority_data
+}
+
 output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
