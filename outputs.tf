dd2084b2afdf8fcb69c8db6cd0994af88329938e
v47 <victor.verg@hotmail.com>
2023-05-01 18:04:53 +0200

    terraform fmt on outputs

--- outputs.tf
+++ outputs.tf
@@ -10,17 +10,17 @@ output "cluster_id" {
 
 output "custer_name" {
   description = "The name of the EKS cluster"
-  value = module.eks.cluster_name
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   description = "Endpoint of the Kubernetes API server"
-  value = module.eks.cluster_endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
-  value = module.eks.cluster_certificate_authority_data
+  value       = module.eks.cluster_certificate_authority_data
 }
 
 output "cluster_oidc_issuer_url" {
