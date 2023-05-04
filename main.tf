a1884bc88fad8e3f925134919ba701e24b1203a9
v47 <victor.verg@hotmail.com>
2023-05-05 00:12:07 +0200

    external-secrets irsa role

--- main.tf
+++ main.tf
@@ -323,7 +323,7 @@ module "external_secrets_irsa" {
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
+      namespace_service_accounts = ["external-secrets:external-secrets"]
     }
   }
 
