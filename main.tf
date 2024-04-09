e3fc817d7fdd62d3a6eb461be4e5a7879a439fe8
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-04-10 04:24:13 +0530

    fix role arn ref

--- main.tf
+++ main.tf
@@ -149,7 +149,7 @@ resource "aws_eks_access_policy_association" "cluster_admin_access" {
   for_each      = { for role in data.aws_iam_roles.account_iam_role : element(split("/", tolist(role.arns)[0]), length(split("/", tolist(role.arns)[0])) - 1) => role }
   cluster_name  = var.cluster_name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = each.value.rolearn
+  principal_arn = each.value.arns[0]
   access_scope {
     type = "cluster"
   }
