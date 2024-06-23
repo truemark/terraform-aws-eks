a5ee33dfc9b6665ecea54a0e23d660c61eb6f5f6
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-06-23 12:51:35 +0530

    enable cluster creator admins permissions

--- main.tf
+++ main.tf
@@ -109,15 +109,16 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.14"
 
-  cluster_name                            = var.cluster_name
-  cluster_version                         = var.cluster_version
-  cluster_endpoint_private_access         = var.cluster_endpoint_private_access
-  cluster_endpoint_public_access          = var.cluster_endpoint_public_access
-  create_cloudwatch_log_group             = var.create_cloudwatch_log_group
-  cluster_enabled_log_types               = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-  cluster_security_group_additional_rules = var.cluster_security_group_additional_rules
-  node_security_group_additional_rules    = var.node_security_group_additional_rules
-  cluster_additional_security_group_ids   = var.cluster_additional_security_group_ids
+  cluster_name                             = var.cluster_name
+  cluster_version                          = var.cluster_version
+  cluster_endpoint_private_access          = var.cluster_endpoint_private_access
+  cluster_endpoint_public_access           = var.cluster_endpoint_public_access
+  create_cloudwatch_log_group              = var.create_cloudwatch_log_group
+  cluster_enabled_log_types                = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  cluster_security_group_additional_rules  = var.cluster_security_group_additional_rules
+  node_security_group_additional_rules     = var.node_security_group_additional_rules
+  cluster_additional_security_group_ids    = var.cluster_additional_security_group_ids
+  enable_cluster_creator_admin_permissions = true
 
   #KMS
   kms_key_users  = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
