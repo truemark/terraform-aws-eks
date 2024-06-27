121e692cb21a152e8a1e27371d75c9424ef0d365
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-06-27 21:02:46 +0530

    pin istio to published module

--- main.tf
+++ main.tf
@@ -511,10 +511,9 @@ module "ingress_traefik" {
 }
 
 module "ingress_istio" {
-  count  = var.enable_istio ? 1 : 0
-  source = "/Volumes/code/truemark/terraform-kubernetes-istio"
-  # source  = "truemark/istio/kubernetes"
-  # version = "~> 0.1"
+  count   = var.enable_istio ? 1 : 0
+  source  = "truemark/istio/kubernetes"
+  version = "~> 0.1"
 
   vpc_id                   = var.vpc_id
   istio_release_version    = var.istio_release_version
