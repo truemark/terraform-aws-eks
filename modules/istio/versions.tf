cd7a462e5687ae18e75999499668af356c7895d9
v47 <victor.verg@hotmail.com>
2024-07-15 18:31:35 +0200

    unified and update providers

--- modules/istio/versions.tf
+++ modules/istio/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2"
+      version = "~> 2.30.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2"
+      version = "~> 2.14.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "~> 1"
+      version = "~> 1.14.0"
     }
   }
 }
--- modules/traefik/versions.tf
+++ modules/traefik/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.10.0"
+      version = "~> 2.30.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.9.0"
+      version = "~> 2.14.0"
     }
   }
 }
