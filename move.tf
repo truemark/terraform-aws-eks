66aba87475f765b2a0bff3d6624723d04f92c81a
Sergii M <115028108+smtm001@users.noreply.github.com>
2024-07-30 20:57:58 +0000

    add move.tf

---
+++ move.tf
@@ -0,0 +1,14 @@
+moved {
+  from = helm_release.karpenter
+  to   = helm_release.karpenter[0]
+}
+
+moved {
+  from = kubectl_manifest.karpenter_node_class
+  to   = kubectl_manifest.karpenter_node_class[0]
+}
+
+moved {
+  from = kubectl_manifest.karpenter_node_pool_arm
+  to   = kubectl_manifest.karpenter_node_pool_arm[0]
+}
