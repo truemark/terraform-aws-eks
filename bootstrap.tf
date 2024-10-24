30cc1800db1291a7b1731660905d57fcfe956c88
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-10-24 18:27:08 +0530

    chore: update bootstrap.tf

---
+++ bootstrap.tf
@@ -0,0 +1,216 @@
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = any
+  default = {
+    enable_aws_load_balancer_controller = true
+    enable_metrics_server               = true
+  }
+}
+
+# Addons Git
+variable "gitops_addons_org" {
+  description = "Git repository org/user contains for addons"
+  type        = string
+  default     = "https://github.com/aws-samples"
+}
+variable "gitops_addons_repo" {
+  description = "Git repository contains for addons"
+  type        = string
+  default     = "eks-blueprints-add-ons"
+}
+variable "gitops_addons_revision" {
+  description = "Git repository revision/branch/ref for addons"
+  type        = string
+  default     = "main"
+}
+variable "gitops_addons_basepath" {
+  description = "Git repository base path for addons"
+  type        = string
+  default     = "argocd/"
+}
+variable "gitops_addons_path" {
+  description = "Git repository path for addons"
+  type        = string
+  default     = "bootstrap/control-plane/addons"
+}
+
+# Workloads Git
+variable "gitops_workload_org" {
+  description = "Git repository org/user contains for workload"
+  type        = string
+  default     = "https://github.com/aws-ia"
+}
+variable "gitops_workload_repo" {
+  description = "Git repository contains for workload"
+  type        = string
+  default     = "terraform-aws-eks-blueprints"
+}
+variable "gitops_workload_revision" {
+  description = "Git repository revision/branch/ref for workload"
+  type        = string
+  default     = "main"
+}
+variable "gitops_workload_basepath" {
+  description = "Git repository base path for workload"
+  type        = string
+  default     = "patterns/gitops/"
+}
+variable "gitops_workload_path" {
+  description = "Git repository path for workload"
+  type        = string
+  default     = "getting-started-argocd/k8s"
+}
+
+locals {
+  gitops_addons_url      = "${var.gitops_addons_org}/${var.gitops_addons_repo}"
+  gitops_addons_basepath = var.gitops_addons_basepath
+  gitops_addons_path     = var.gitops_addons_path
+  gitops_addons_revision = var.gitops_addons_revision
+
+  gitops_workload_url      = "${var.gitops_workload_org}/${var.gitops_workload_repo}"
+  gitops_workload_basepath = var.gitops_workload_basepath
+  gitops_workload_path     = var.gitops_workload_path
+  gitops_workload_revision = var.gitops_workload_revision
+
+  aws_addons = {
+    enable_cert_manager                          = try(var.addons.enable_cert_manager, false)
+    enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, false)
+    enable_aws_fsx_csi_driver                    = try(var.addons.enable_aws_fsx_csi_driver, false)
+    enable_aws_cloudwatch_metrics                = try(var.addons.enable_aws_cloudwatch_metrics, false)
+    enable_aws_privateca_issuer                  = try(var.addons.enable_aws_privateca_issuer, false)
+    enable_cluster_autoscaler                    = try(var.addons.enable_cluster_autoscaler, false)
+    enable_external_dns                          = try(var.addons.enable_external_dns, false)
+    enable_external_secrets                      = try(var.addons.enable_external_secrets, false)
+    enable_aws_load_balancer_controller          = try(var.addons.enable_aws_load_balancer_controller, false)
+    enable_fargate_fluentbit                     = try(var.addons.enable_fargate_fluentbit, false)
+    enable_aws_for_fluentbit                     = try(var.addons.enable_aws_for_fluentbit, false)
+    enable_aws_node_termination_handler          = try(var.addons.enable_aws_node_termination_handler, false)
+    enable_karpenter                             = try(var.addons.enable_karpenter, false)
+    enable_velero                                = try(var.addons.enable_velero, false)
+    enable_aws_gateway_api_controller            = try(var.addons.enable_aws_gateway_api_controller, false)
+    enable_aws_ebs_csi_resources                 = try(var.addons.enable_aws_ebs_csi_resources, false)
+    enable_aws_secrets_store_csi_driver_provider = try(var.addons.enable_aws_secrets_store_csi_driver_provider, false)
+    enable_ack_apigatewayv2                      = try(var.addons.enable_ack_apigatewayv2, false)
+    enable_ack_dynamodb                          = try(var.addons.enable_ack_dynamodb, false)
+    enable_ack_s3                                = try(var.addons.enable_ack_s3, false)
+    enable_ack_rds                               = try(var.addons.enable_ack_rds, false)
+    enable_ack_prometheusservice                 = try(var.addons.enable_ack_prometheusservice, false)
+    enable_ack_emrcontainers                     = try(var.addons.enable_ack_emrcontainers, false)
+    enable_ack_sfn                               = try(var.addons.enable_ack_sfn, false)
+    enable_ack_eventbridge                       = try(var.addons.enable_ack_eventbridge, false)
+  }
+  oss_addons = {
+    enable_argocd                          = try(var.addons.enable_argocd, false)
+    enable_argo_rollouts                   = try(var.addons.enable_argo_rollouts, false)
+    enable_argo_events                     = try(var.addons.enable_argo_events, false)
+    enable_argo_workflows                  = try(var.addons.enable_argo_workflows, false)
+    enable_cluster_proportional_autoscaler = try(var.addons.enable_cluster_proportional_autoscaler, false)
+    enable_gatekeeper                      = try(var.addons.enable_gatekeeper, false)
+    enable_gpu_operator                    = try(var.addons.enable_gpu_operator, false)
+    enable_ingress_nginx                   = try(var.addons.enable_ingress_nginx, false)
+    enable_kyverno                         = try(var.addons.enable_kyverno, false)
+    enable_kube_prometheus_stack           = try(var.addons.enable_kube_prometheus_stack, false)
+    enable_metrics_server                  = try(var.addons.enable_metrics_server, false)
+    enable_prometheus_adapter              = try(var.addons.enable_prometheus_adapter, false)
+    enable_secrets_store_csi_driver        = try(var.addons.enable_secrets_store_csi_driver, false)
+    enable_vpa                             = try(var.addons.enable_vpa, false)
+  }
+  addons = merge(
+    local.aws_addons,
+    local.oss_addons,
+    { kubernetes_version = var.cluster_version },
+    { aws_cluster_name = module.eks.cluster_name }
+  )
+
+  addons_metadata = merge(
+#     module.eks_addons.gitops_metadata,
+    {
+      aws_cluster_name = module.eks.cluster_name
+      aws_region       = data.aws_region.current.name
+      aws_account_id   = data.aws_caller_identity.current.account_id
+      aws_vpc_id       = var.vpc_id
+    },
+    {
+      addons_repo_url      = local.gitops_addons_url
+      addons_repo_basepath = local.gitops_addons_basepath
+      addons_repo_path     = local.gitops_addons_path
+      addons_repo_revision = local.gitops_addons_revision
+    },
+    {
+      workload_repo_url      = local.gitops_workload_url
+      workload_repo_basepath = local.gitops_workload_basepath
+      workload_repo_path     = local.gitops_workload_path
+      workload_repo_revision = local.gitops_workload_revision
+    }
+  )
+
+  tags = {
+    Blueprint  = var.cluster_name
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+
+  argocd_apps = {
+    eks-addons = {
+      project = "default"
+      repo_url = "https://github.com/truemark/terraform-aws-eks"
+      target_revision = "feat/argocd"
+      path = "bootstrap/charts/eks-addons"
+      values = {
+        certManager = merge({
+          enabled = var.addons_enable_cert_manager
+        }, var.cert_manager_helm_config)
+      }
+    }
+#     workloads = file("${path.module}/bootstrap/workloads.yaml")
+  }
+}
+
+################################################################################
+# GitOps Bridge: Bootstrap
+################################################################################
+variable "enable_gitops_bridge_bootstrap" {
+  default = true
+}
+
+module "gitops_bridge_bootstrap" {
+  count = var.enable_gitops_bridge_bootstrap ? 1 : 0
+  source = "./modules/argocd-gitops-bridge"
+
+  cluster = {
+    metadata     = local.addons_metadata
+    addons       = local.addons
+  }
+  argocd = {
+    chart_version = "7.6.10"
+    values        = [
+      <<-EOT
+    global:
+      nodeSelector:
+        ${jsonencode(var.critical_addons_node_selector)}
+      tolerations:
+        ${jsonencode(var.critical_addons_node_tolerations)}
+    EOT
+    ]
+  }
+  apps = local.argocd_apps
+}
+
+# ################################################################################
+# # EKS Blueprints Addons
+# ################################################################################
+# module "eks_addons" {
+#   source  = "./modules/eks-addons"
+#
+#   cluster_name      = module.eks.cluster_name
+#   cluster_endpoint  = module.eks.cluster_endpoint
+#   cluster_version   = module.eks.cluster_version
+#   oidc_provider_arn = module.eks.oidc_provider_arn
+#
+#   # Using GitOps Bridge
+#   create_kubernetes_resources = false
+#
+#   # EKS Blueprints Addons
+#   enable_cert_manager                 = local.aws_addons.enable_cert_manager
+#
+#   tags = local.tags
+# }
