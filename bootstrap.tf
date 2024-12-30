3aceb12577555a1b6e16034e3d0f4b0356bf9214
Sergii M <115028108+smtm001@users.noreply.github.com>
2024-12-30 13:33:59 +0000

    init commit

--- bootstrap.tf
+++ bootstrap.tf
@@ -125,13 +125,14 @@ locals {
           ingress         = var.istio_helm_config.ingress
         }
         velero = {
-          enabled      = local.addons.enable_velero
-          iamRoleArn   = try(module.addons.gitops_metadata.velero_iam_role_arn, "")
-          values       = try(yamldecode(join("\n", var.velero_helm_config.values)), {})
-          bucket       = module.addons.gitops_metadata.velero_backup_s3_bucket_name
-          prefix       = module.addons.gitops_metadata.velero_backup_s3_bucket_prefix
-          region       = data.aws_region.current.id
-          chartVersion = try(var.velero_helm_config.chart_version, "8.0.0")
+          enabled            = local.addons.enable_velero
+          iamRoleArn         = try(module.addons.gitops_metadata.velero_iam_role_arn, "")
+          values             = try(yamldecode(join("\n", var.velero_helm_config.values)), {})
+          bucket             = try(module.addons.gitops_metadata.velero_backup_s3_bucket_name, null)
+          prefix             = try(module.addons.gitops_metadata.velero_backup_s3_bucket_prefix, null)
+          serviceAccountName = try(module.addons.gitops_metadata.velero_service_account_name, null)
+          region             = data.aws_region.current.id
+          chartVersion       = try(var.velero_helm_config.chart_version, "8.0.0")
         }
         castAi = {
           enabled   = local.addons.enable_cast_ai
