a89a0e917c80774d99c6487cba6e3e1a2907de39
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2025-08-15 21:18:43 +0530

    temp irsa fix

--- modules/addons/kube_bench/main.tf
+++ modules/addons/kube_bench/main.tf
@@ -11,7 +11,7 @@ locals {
 # IAM role for Security Hub access
 module "kube_bench_irsa_role" {
   source           = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version          = "~> 5"
+  version          = "~> 5.0"
   role_name_prefix = "kube-bench-"
   oidc_providers = {
     eks = {
